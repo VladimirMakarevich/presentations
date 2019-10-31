7053a878a0d3b5a2dd57624ba7b7ca2e38fbcc61
Vladimir Makarevich <makarevich.dev@outlook.com>
2019-10-31 03:41:33 +0300

    reactiveX: work on the win form sample

---
+++ reactiveX/examples/Forms.ReactiveX/Forms.ReactiveX/MessageBus/Contracts/MessageBase.cs
@@ -0,0 +1,8 @@
+﻿using Forms.ReactiveX.MessageBus.Types;
+
+namespace Forms.ReactiveX.MessageBus.Contracts {
+    public abstract class MessageBase {
+        public abstract string Name { get; }
+        public abstract ChannelType Type { get; }
+    }
+}
---
+++ reactiveX/examples/Forms.ReactiveX/Forms.ReactiveX/MessageBus/LazyBus.cs
@@ -0,0 +1,25 @@
+﻿using Forms.ReactiveX.MessageBus.Contracts;
+using System;
+
+namespace Forms.ReactiveX.MessageBus
+{
+    public static class Ioc {
+        public static IMessageBus GetBus() {
+            return new MessageBus();
+        }
+    }
+
+    public sealed class LazyBus {
+        public IMessageBus Bus { get; }
+
+        private static readonly Lazy<LazyBus> Instance =
+            new Lazy<LazyBus>(
+                () => new LazyBus());
+
+        LazyBus() {
+            Bus = new MessageBus();
+        }
+
+        public static LazyBus GetInstance => Instance.Value;
+    }
+}
